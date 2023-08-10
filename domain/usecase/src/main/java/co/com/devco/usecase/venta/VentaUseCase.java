9c6ec2a8aeef8aafb40ba56a8be041d05d23f520
Felipe Cristancho <icristan@bancolombia.com.co>
2023-08-10 11:44:07 -0500

    Se expone endpoint para mostrar una venta por su id

--- domain/usecase/src/main/java/co/com/devco/usecase/venta/VentaUseCase.java
+++ domain/usecase/src/main/java/co/com/devco/usecase/venta/VentaUseCase.java
@@ -4,6 +4,7 @@ import co.com.devco.model.cliente.gateways.ClienteRepository;
 import co.com.devco.model.venta.Venta;
 import co.com.devco.model.venta.gateways.VentaRepository;
 import co.com.devco.usecase.dto.VentaDto;
+import co.com.devco.usecase.exception.ExcepcionNoEncontrado;
 import co.com.devco.usecase.mapper.VentaMapper;
 import lombok.RequiredArgsConstructor;
 
@@ -18,5 +19,11 @@ public class VentaUseCase {
     public List<VentaDto> obtenerVentas(){
         List<Venta> venta = this.ventaRepository.obtenerVentas();
         return VentaMapper.toListVentaDto(venta);
+    }
+
+    public VentaDto obtenerVentaPorId(Long id){
+        Venta venta = this.ventaRepository.obtenerVenta(id).
+                orElseThrow(() -> new ExcepcionNoEncontrado("Venta no encontada"));
+        return VentaMapper.toVentaDto(venta);
     }
 }
--- infrastructure/entry-points/api-rest/src/main/java/co/com/devco/api/VentaController.java
+++ infrastructure/entry-points/api-rest/src/main/java/co/com/devco/api/VentaController.java
@@ -6,6 +6,7 @@ import co.com.devco.usecase.venta.VentaUseCase;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,10 @@ public class VentaController {
     @GetMapping
     public List<VentaDto> obtenerVentas(){
         return this.useCase.obtenerVentas();
+    }
+
+    @GetMapping("/{id}")
+    public VentaDto obtenerVentaPorId(@PathVariable Long id){
+        return this.useCase.obtenerVentaPorId(id);
     }
 }
